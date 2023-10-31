97fe5d929aa0f1099e6491654353aa4c9708688b
Shubham Paudel <shubhampaudel7@gmail.com>
2023-10-31 21:18:07 +0545

    Profile-app

--- profileapp/lib/main.dart
+++ profileapp/lib/main.dart
@@ -11,9 +11,14 @@ void main() {
       body: Column(children: [
        // SizedBox(height: 10),
         Center(child: Image.network("https://cdn.dribbble.com/users/6229959/screenshots/14421439/media/1d3513f47589f4aa40ac6d3064d436bd.png?resize=1000x750&vertical=center", height: 300, width: 500)),
-        SizedBox(height: 10),
-        Text('Name: Roronoa Zoro'),
-        
+        const SizedBox(height: 10),
+        const Text('Name: Roronoa Zoro', style: TextStyle(fontSize: 24,color: Colors.green, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 5),
+        const Text("Address: Grand line",style: TextStyle(fontSize: 19)),
+        const SizedBox(height: 5,),
+        const Text('Email: shubhampaudel7@gmail.com',style: TextStyle(fontSize: 19)),
+        const SizedBox(height: 90),
+        const Text('Developed by: Shubham Raj Paudel',style:TextStyle(fontWeight: FontWeight.bold)),
       ],),
       )
     ,));
