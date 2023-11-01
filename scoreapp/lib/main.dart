9314a16bff82b7c867ba88fec1f18e125ae00d72
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-01 20:10:18 +0545

    Scoreapp

--- scoreapp/lib/main.dart
+++ scoreapp/lib/main.dart
@@ -2,8 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const ScoreApp());
 }
 
@@ -17,7 +16,7 @@ class ScoreApp extends StatelessWidget {
       title: "Score App",
       home: ScoreHome(),
     );
-}
+  }
 }
 
 class ScoreHome extends StatefulWidget {
@@ -28,26 +27,79 @@ class ScoreHome extends StatefulWidget {
 }
 
 class _ScoreHomeState extends State<ScoreHome> {
+  int number = 0;
+
+  void increasenumber() {
+    setState(() {
+      number++;
+    });
+  }
+
+  void decreasenumber() {
+    setState(() {
+      number--;
+    });
+  }
+
+  void resetnumber() {
+    setState(() {
+      number = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: const Text("Score Changer"),
-      centerTitle: true,
-    ),
-
-    body:  Column(
-      children: [
-        SizedBox(height: 25),
-        Center(child: Text("Score is", style: TextStyle(color: Colors.green, fontSize: 25, fontWeight: FontWeight.bold)),
-         ),
-         SizedBox(height: 25,),
-         Center(child: Text("0", style: TextStyle(color: Colors.green, fontSize: 85, fontWeight: FontWeight.bold)))
-         ,
-         SizedBox(height: 25,),
-         ElevatedButton(child: Text('Increase'),onPressed :(){}),
-      ],
-    ),
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Score Changer"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: resetnumber,
+        child: Icon(Icons.restore_outlined),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 25),
+          Center(
+            child: Text("Score is",
+                style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold)),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Center(
+              child: Text(number.toString(),
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 125,
+                      fontWeight: FontWeight.bold))),
+          SizedBox(
+            height: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: increasenumber,
+                child: Text('Increase'),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: decreasenumber,
+                child: Text('Decrease'),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
