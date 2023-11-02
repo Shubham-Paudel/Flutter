25ab41145f12c09cefdb126ac18b890fff46b61b
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-02 18:03:09 +0545

    todolist

---
+++ todolist/lib/Screens/home.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:todolist/constants/Colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     home: Scaffold(
+        
+        appBar: AppBar(
+            backgroundColor: tdBGColor,
+            title: Row(
+              children: [
+                Icon(
+                  Icons.menu,
+                  color: tdBlack,
+                   size: 30)
+              ],
+            )),
+        body: Container(
+          child: Text('This is home screen'),
+        )));
+  }
+}
---
+++ todolist/lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color.fromARGB(255, 8, 0, 255);
+
+const Color tdBlack = Color.fromARGB(255, 0, 0, 0);
+
+const Color tdGrey = Color.fromARGB(255, 80, 80, 80);
+
+const Color tdBGColor = Color.fromARGB(255, 255, 255, 255);
+
