2908655f10c9b404b24e6720e5bb84b5ab6eea62
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-07 20:27:47 +0545

    tictactoe

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp( 
       debugShowCheckedModeBanner: false,
+      color: Colors.white,
     );
   }
 }
