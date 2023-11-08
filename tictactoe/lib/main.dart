435b8120ce60487ac470f04c8ee6642dc3bfe048
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-08 18:12:38 +0545

    tictactoe

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +11,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      color: Colors.white,
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String displayExOh ='';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[800],
+        body: GridView.builder(
+            itemCount: 9,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: _tapped,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: Center(
+                  child: Text(
+                    displayExOh,
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ),
+              )
+              );
+            }));
+  }
+  void _tapped()
+  {
+    setState(() {
+        displayExOh='O';
+    });
   }
 }
