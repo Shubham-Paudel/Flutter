818f8c4358d13b6465d83c9869f115504327c28d
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-20 13:26:40 +0545

    flappybird

--- flappybird/lib/home_page.dart
+++ flappybird/lib/home_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'bird.dart';
 
@@ -11,17 +13,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  double birdYaxis = 0;
+
+  void jump()
+  {
+    Timer.periodic(Duration(milliseconds: 500), (timer) { 
+      setState(() {
+      birdYaxis += 0.1;
+    });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
         Expanded(
           flex: 2,
-          child: AnimatedContainer(
-            alignment: Alignment(-1, 1),
-            duration: Duration(milliseconds: 0),
-            color: Colors.blue,
-            child: MyBird(),
+          child: GestureDetector(
+            onTap: jump,
+            child: AnimatedContainer(
+              alignment: Alignment(0, birdYaxis),
+              duration: Duration(milliseconds: 0),
+              color: Colors.blue,
+              child: MyBird(),
+            ),
           ),
         ),
         Expanded(
