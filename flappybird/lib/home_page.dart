b9a3bd3b27fbf4b66eb31daf8fb776cb7dd38497
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-20 13:16:46 +0545

    flappybird

--- flappybird/lib/home_page.dart
+++ flappybird/lib/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'bird.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +13,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(  
-      
+    return Scaffold(
+      body: Column(children: [
+        Expanded(
+          flex: 2,
+          child: AnimatedContainer(
+            alignment: Alignment(-1, 1),
+            duration: Duration(milliseconds: 0),
+            color: Colors.blue,
+            child: MyBird(),
+          ),
+        ),
+        Expanded(
+            child: Container(
+          color: Colors.green,
+        ))
+      ]),
     );
   }
 }
