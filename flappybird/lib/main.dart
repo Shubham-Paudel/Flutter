e8516ceed3c2620603aa32c194df9228ebeb2304
Shubham Paudel <shubhampaudel7@gmail.com>
2023-11-20 12:54:11 +0545

    flappybird

--- flappybird/lib/main.dart
+++ flappybird/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp( 
       debugShowCheckedModeBanner: false,
-      home: Scaffold( 
-        backgroundColor: Colors.white,
-      ),
+      home: HomePage(),
     );
   }
 }
